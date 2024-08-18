9e71210d6a73f816843845d29a7868f486b95acf
LeShaDDoW <leshaddow3@gmail.com>
2024-08-18 20:16:16 +0200

    finished it (basically)

--- Assets/PuzzleRooms/Scripts/Webhooks.cs
+++ Assets/PuzzleRooms/Scripts/Webhooks.cs
@@ -1,3 +1,4 @@
+using ModWobblyLife;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,7 +12,6 @@ public class Webhooks : MonoBehaviour
 
     private IEnumerator UpdateCounter()
     {
-        // Step 1: Get the current counter value from the server
         UnityWebRequest counterRequest = UnityWebRequest.Get("https://lstwo.net/puzzlerooms/count_training_steps.php");
         yield return counterRequest.SendWebRequest();
 
@@ -20,14 +20,37 @@ public class Webhooks : MonoBehaviour
             yield break;
         }
 
-        // Step 2: Parse the response
         string jsonResponse = counterRequest.downloadHandler.text;
         int counterValue = int.Parse(jsonResponse.Split(':')[1].Replace("}", "").Trim());
 
-        // Step 3: Format the message with the counter value
-        string message = $"Training Data Collection Step: {counterValue}!";
+        string message = $"Training Data Collection Step: {counterValue}!\n";
 
-        // Step 4: Send the message to Discord
+        foreach(ModPlayerController player in ModInstance.Instance.GetModPlayerControllers())
+        {
+            message = message + player.GetPlayerName() + "\n";
+        }
+
+        WWWForm form = new WWWForm();
+        form.AddField("content", message);
+
+        UnityWebRequest discordRequest = UnityWebRequest.Post("https://discord.com/api/webhooks/1274715345881665616/diQwjqmsadrWnGF59_WHRIBlnL11ilEhrtOSHUHNOByb238NVVEa1JloKmgfsgWH_j4u", form);
+        yield return discordRequest.SendWebRequest();
+    }
+
+    public void SendStart()
+    {
+        StartCoroutine(SendStart_Coroutine());
+    }
+
+    private IEnumerator SendStart_Coroutine()
+    {
+        string message = $"New Data Collection Subjects!\n";
+
+        foreach (ModPlayerController player in ModInstance.Instance.GetModPlayerControllers())
+        {
+            message = message + player.GetPlayerName() + "\n";
+        }
+
         WWWForm form = new WWWForm();
         form.AddField("content", message);
 
